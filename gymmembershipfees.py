49f3d2d8522dc11e13df658666b12315c50fdff0
theclw94 <67998734+theclw94@users.noreply.github.com>
2020-07-08 15:25:45 +0800

    Update gymmembershipfees.py

--- gymmembershipfees.py
+++ gymmembershipfees.py
@@ -1,12 +1,14 @@
 # The fees for entering the gym, in RM. The monthly membership costs RM 70, per-entry fee
 # is RM 7
+
 while True:
     print("")
-    fee_monthly=int(input("Gym monthly membership fee: "))
-    fee_once=int(input("Gym single entry fee: "))
+    fee_monthly = int(input("Gym monthly membership fee: "))
+    fee_once = int(input("Gym single entry fee: "))
 
 # First, ask yourself, how often would you go to the gym. Second, how many days
 # are there in a month? Third, How many days in a month will you go to the gym?
+    
     gym_enter_interval = int(input("Go to gym once per how many days?: "))
     month_days = 30
     gym_enter_freq = month_days/gym_enter_interval
@@ -15,6 +17,7 @@ while True:
 
 # Determine how much you'll be paying in a month,
 # and compare that to the monthly fee.
+    
     fee = gym_enter_freq * fee_once
     if fee >= fee_monthly:
         print("It is recommended to take up the monthly membership.")
